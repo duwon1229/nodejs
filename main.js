46efb3ba275f8c3425296194f012a3d0c6894086
duwon1229 <endndnjs2@naver.com>
2026-05-06 17:20:04 +0900

    home router 추가

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = express();
 const fs = require("fs");
 var bodyParser = require("body-parser");
 var compression = require("compression");
-var template = require("./lib/template.js");
 var topciRouter = require("./routes/topic");
+var indexRouter = require("./routes/index");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,23 +18,8 @@ app.get("/{*any}", function (request, response, next) {
   });
 });
 
+app.use("/", indexRouter);
 app.use("/topic", topciRouter);
-
-// route, routing
-app.get("/", (request, response) => {
-  var title = "Welcome";
-  var description = "Hello, Node.js";
-  var list = template.list(request.list);
-  var html = template.HTML(
-    title,
-    list,
-    `<h2>${title}</h2>${description}
-    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px" />
-    `,
-    `<a href="/topic/create">create</a>`,
-  );
-  response.send(html);
-});
 
 app.use(function (request, response, next) {
   response.status(404).send("Sorry can't find that!");
