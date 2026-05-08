2a70124dbfeaac19c20cef1d527b8d4ea1b8e3c4
duwon1229 <endndnjs2@naver.com>
2026-05-08 16:46:12 +0900

    permanent cookie

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -5,9 +5,15 @@ http
   .createServer(function (request, response) {
     var cookies = cookie.parse(request.headers.cookie || "");
     console.log(cookies.yummy_cookie);
+    // Session cookie
     // response.writeHead(200, {
     //   "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"],
     // });
+
+    //Permanent cookie
+    response.writeHead(200, {
+      "Set-Cookie": [`Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`],
+    });
 
     response.end("Cookie!!");
   })
